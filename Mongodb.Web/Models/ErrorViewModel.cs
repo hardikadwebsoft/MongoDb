c8b6bb37efdda6a32819f1c985c5c1675169d010
Hardik Gohil <hardik87.gohil@hotmail.com>
2020-08-29 10:31:29 +0530

    Initial commit.

---
+++ Mongodb.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mongodb.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
